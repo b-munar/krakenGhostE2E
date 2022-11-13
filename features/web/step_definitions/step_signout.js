df3535c0b4ee65dd3d687c0dbf2b24093f9adf3a
Brahian Munar <b.munar@uniandes.edu.co>
2022-11-13 13:09:34 -0500

    ninth commit

--- features/web/step_definitions/step_signout.js
+++ features/web/step_definitions/step_signout.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 
 
 When('I click in Profile', async function(){
-    let element = await this.driver.$('#ember47');
+    let element = await this.driver.$('div[class="ember-view ember-basic-dropdown-trigger  flex items-center outline-0 pointer space-between pa2 pl4 pr3 mt3 mb6"]');
     return await element.click();
   })
 
